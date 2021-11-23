48d669e508c11b67bdbae7d7e4c869da10e8a66f
Aripiyok <86340085+Aripiyok@users.noreply.github.com>
2021-11-23 18:20:59 +0700

    Update __main__.py

--- EmiliaAnimeBot/__main__.py
+++ EmiliaAnimeBot/__main__.py
@@ -87,7 +87,7 @@ buttons = [
     [
         InlineKeyboardButton(text="••• ᴜᴘᴅᴀᴛᴇs ••", url="https://t.me/fl0werboy"),
         InlineKeyboardButton(
-            text="•• ᴍᴜsɪᴄ ᴘʟᴀʏᴇʀ ••", url=f"https://telegra.ph/COMMANDS-BOYTAPIBOT-11-17"
+            text="•• ᴍᴜsɪᴄ ᴘʟᴀʏᴇʀ ••", url=f"https://telegra.ph/Users-Commands-11-23"
         ),
     ],    
     [
