8f3f40507ca8c736e995700ef48f02853af97904
Ariav <86340085+Aripiyok@users.noreply.github.com>
2022-01-15 23:25:44 +0700

    Update imagefiles.py

--- EmiliaAnimeBot/resources/imagefiles.py
+++ EmiliaAnimeBot/resources/imagefiles.py
@@ -1,6 +1,6 @@
 # This File Contains all the Picture Links used all over the Bot
 
 #Images in __main__.py
-EMILIA_IMG = "https://telegra.ph/file/f083f40188aba90032200.jpg"
+EMILIA_IMG = "https://telegra.ph/file/980b0d49ac05cdd629687.jpg"
 EMILIA_START_IMG = "https://telegra.ph/file/980b0d49ac05cdd629687.jpg"
-EMILIA_HELP_IMG = "https://telegra.ph/file/f083f40188aba90032200.jpg"
+EMILIA_HELP_IMG = "https://telegra.ph/file/980b0d49ac05cdd629687.jpg"
